df4b2842c8f7f560c7fe0d9908afc39e99b4c399
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-02-03 22:31:21 +0200

    day 3.1

---
+++ functions.js
@@ -0,0 +1,25 @@
+function introduce(name, age) {
+    console.log("Привет! Меня зовут " + name + ", мне " + age + " лет.");
+}
+
+function square(number) {
+    return number * number;
+}
+
+introduce("Алексей", 30);
+console.log("Квадрат числа 5: " + square(5));
+
+function average(a, b, c) {
+    return (a + b + c) / 3
+};
+console.log(average(4, 5, 6));
+
+function para(num) {
+    if (num % 2 == 1) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+console.log(para(16))
