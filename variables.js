91902fa4764ac25e74e79a2375c736f070a9b730
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-02-03 21:58:01 +0200

    day 2

--- variables.js
+++ variables.js
@@ -1,4 +1,8 @@
-let userName = "Андрей";
-let userAge = 25;
+let userName = "Петя";
+let userAge = 18;
+const userHappy = true
+const userGroup = {sisters: "Аня, Таня", brothers: "Коля, Вася"}
 console.log("Имя: " + userName);
 console.log("Возраст: " + userAge);
+console.log(userName + " счастлив? " + userHappy)
+console.log("У " +  userName + " есть сестры : " + userGroup.sisters)
