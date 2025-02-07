133bfcf359e81ee77c12143fe753c99d1636957d
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-02-07 16:53:20 +0200

    day 9 test

--- objectMethods.js
+++ objectMethods.js
@@ -4,28 +4,23 @@ let person = {
     gender: "male"
 };
 
-// Копирование объекта
+
 let personCopy = Object.assign({}, person);
 personCopy.age = 50;
 
 console.log("Оригинал:", person);
 console.log("Копия:", personCopy);
 
-// Объединение объектов
+
 let additionalInfo = { country: "Romania", occupation: "Engineer" };
 let mergedPerson = Object.assign({}, person, additionalInfo);
 
 console.log("Объединенный объект:", mergedPerson);
 
-// Перебор объекта
+
 console.log("Ключи:", Object.keys(mergedPerson));
 console.log("Значения:", Object.values(mergedPerson));
 console.log("Ключ-значение:", Object.entries(mergedPerson));
-
-// 1️⃣ Создай объект car и добавь к нему свойства brand, model, year.
-// 2️⃣ Скопируй объект car, измени year, выведи оба объекта.
-// 3️⃣ Добавь к car новый объект с color и owner (используй Object.assign).
-// 4️⃣ Выведи Object.keys(), Object.values() и Object.entries() для car.
 
 let car = {};
 car.brand = "Toyota";
@@ -44,3 +39,6 @@ console.log("Ключ-значение:", Object.entries(car));
 console.log("Ключи:", Object.keys(fullCar));
 console.log("Значения:", Object.values(fullCar));
 console.log("Ключ-значение:", Object.entries(fullCar));
+
+let deepCopyCar = structuredClone(fullCar);
+console.log(deepCopyCar);
