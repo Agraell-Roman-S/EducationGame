00d1355fbddc5a6fd38bb67e447c0b3899d946c5
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-02-07 14:14:51 +0200

    day 8 test

---
+++ objects.js
@@ -0,0 +1,27 @@
+let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+
+console.log(`Марка: ${car.brand}, Модель: ${car.model}, Год: ${car.year}`);
+
+car.year = 2022; // Обновляем год
+car.color = "Черный"; // Добавляем новый параметр
+
+console.log("Обновленные данные:");
+for (let key in car) {
+    console.log(`${key}: ${car[key]}`);
+}
+
+let person = {
+    name: "Jon",
+    age: 45,
+    gender: "male"
+};
+
+person.country = "Romania";
+
+for (let i in person) {
+    console.log(`${i}: ${person[i]}`)
+}
