e033e896a178a62368a512189383df1e7da5a764
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-02-09 11:03:17 +0200

    day 12 test

---
+++ spreadRest.js
@@ -0,0 +1,31 @@
+let numbers1 = [10, 20, 30, 40, 50];
+
+// Spread: создание нового массива
+let extendedNumbers = [...numbers1, 60, 70];
+console.log("Расширенный массив:", extendedNumbers);
+
+// Spread: объединение объектов
+let person = { name: "Bob", age: 30 };
+let personWithJob = { ...person, job: "Engineer" };
+console.log("Объединенный объект:", personWithJob);
+
+// Rest: функция суммирования
+function sum(...nums) {
+    return nums.reduce((acc, num) => acc + num, 0);
+}
+
+console.log("Сумма чисел:", sum(5, 10, 15, 20));
+
+let numbers = [10, 20, 30, 40, 50];
+let newNumbers  = [...numbers];
+
+let user = { name: "Bob", age: 30 };
+let adress = {city: "Londn"};
+let fullUser = {...user, ...adress};
+console.log(fullUser);
+
+function multiply(...something) {
+    return something.reduce((acc, j)=>acc * j)
+};
+
+console.log(multiply(1, 6, 4, 3))
