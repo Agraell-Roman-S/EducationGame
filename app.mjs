3b50139195ab19e0c1af334338416033d7a39a29
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-03-02 16:43:33 +0200

    day 15 bonus

--- app.mjs
+++ app.mjs
@@ -11,3 +11,17 @@ console.log(randomNumber(10, 15));
 console.log(objUs.age)
 objUs.age = randomNumber(12, 18)
 console.log(objUs.name, " new age", objUs.age)
+
+async function dynamicImportExample() {
+    let randomValue = Math.random();
+    
+    if (randomValue > 0.5) {
+        const {add} = await import('./math.mjs');
+        add(1,5);
+        console.log('Модуль успешно загружен.');
+    } else {
+        console.log('Модуль не загружен, так как значение меньше 0.5.');
+    }
+}
+
+dynamicImportExample();
