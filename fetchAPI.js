c3d6908e2e719aaf435c0beeca05369ec425d878
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-03-09 19:48:13 +0200

    day 17 test

---
+++ fetchAPI.js
@@ -0,0 +1,99 @@
+async function fetchUsers() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await response.json();
+    console.log("👥 Список пользователей:", users);
+}
+
+async function sendUserData() {
+    let newUser = { name: "John Doe", email: "john@example.com" };
+
+    let response = await fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUser)
+    });
+
+    let data = await response.json();
+    console.log("✅ Пользователь создан:", data);
+}
+
+
+async function getUsers() {
+    try {let resp = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await resp.json();
+    console.log(users)}
+    catch (error){
+        console.log(error)
+    }
+}
+
+async function getUser(userId){
+    try {
+        let resp = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        let result = await resp.json();
+        console.log(result)
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+async function createUser(name, email) {
+    let newUser =  {name: name, email: email};
+    try  {
+        let res = await fetch (url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newUser)
+        });
+            let result = await res.json();
+            console.log("result: user id 11");
+    }
+    catch (error){
+        console.log(error)
+    };
+}
+
+
+async function updateUser(userId, newData) {
+    try {
+        let resp = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newData)
+        });
+        let result = await resp.json();
+        console.log("пользователь ", userId, "обновлен", result)
+    } 
+    catch(err){console.log(err)}
+}
+
+async function deleteUser(userId) {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {
+        method: "DELETE"
+    });
+
+    if (response.ok) {
+        console.log("✅ Пользователь удален!");
+    } else {
+        console.error("Ошибка удаления");
+    }
+}
+
+deleteUser(1);
+
+
+let updUser = {name: "Marco 2", email: "marco2@gmail.com"};
+
+async function main() {
+    await getUsers();
+    await createUser("Marco", "marco@gmail.com");
+    await getUser(11);
+    await updateUser(11, updUser);
+    await getUser(9);
+
+};
+
+main();
