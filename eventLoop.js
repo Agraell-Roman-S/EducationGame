a97dcf1de05d3a4aebe4bf6d6f2cc8173c952174
roman.sozykin.delicloud@gmail.com <roman.sozykin.delicloud@gmail.com>
2025-03-14 15:03:20 +0200

    dfay 19 test

---
+++ eventLoop.js
@@ -0,0 +1,12 @@
+console.log("should be first");
+
+let stt = setTimeout(()=>console.log("should be fourth"), 0);
+
+
+Promise.resolve().then(()=>{
+    console.log("should be second")
+})
+
+queueMicrotask(()=>{
+    console.log ("should be treed")
+})
